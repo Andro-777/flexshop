526994dc2c73766e0de44ed0f4424bada06bdf29
Andro-777 <60812631+Andro-777@users.noreply.github.com>
2020-02-08 14:21:36 +0400

    `12
    instagram shop

---
+++ Js/index.js
@@ -0,0 +1,6 @@
+document.getElementById("dolin").onclick = function(){
+    window.open('https://www.instagram.com/flex._.shop_________/','_self')
+}
+document.getElementById("dolin2").onclick = function(){
+    window.open('https://www.instagram.com/flex._.shop_________/','_self')
+}
---
+++ ss
@@ -0,0 +1,4 @@
+https://giphy.com/gifs/animation-walt-disney-mickey-mouse-l2JhL0Gpfbvs4Y07K
+https://giphy.com/gifs/disney-mickey-mouse-rY8LZ8UmAIruM/links
+https://giphy.com/gifs/mickey-mouse-2sM0zOTQ2tjKo/links
+
